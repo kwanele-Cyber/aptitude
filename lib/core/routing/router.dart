58feed2a96a9acd091223953eb22fb31e9f707bf
zikhalikwanele44 <zikhalikwanele44@gmail.com>
2026-04-19 07:24:31 +0000

    setup firebase in lib/main.dart file

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -9,7 +9,7 @@ final GoRouter router = GoRouter(
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return const LandingPage();
+        return const LoginPage();
       },
     ),
     GoRoute(
