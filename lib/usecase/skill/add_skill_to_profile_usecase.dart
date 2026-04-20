92ae32bd11751df1d373368355f7667bb6f5c39e
kwanele-Cyber <52756699+kwanele-Cyber@users.noreply.github.com>
2026-04-20 09:06:21 +0200

    Add review notes in add_skill_to_profile_usecase.dart
    Add review notes for database logic and data schema compatibility.

--- lib/usecase/skill/add_skill_to_profile_usecase.dart
+++ lib/usecase/skill/add_skill_to_profile_usecase.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AddSkillToProfileUseCase {
+  //ReviewNotes: Please use the created in lib/core/services/firebase_service.dart to centralize all data base related logic.
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  //ReviewNotes: The DataSchema used here is not compatible with the model for lib/core/data/models/skill.dart
+  //to query
   Future<void> execute({
     required String userId,
     required String skillName,
