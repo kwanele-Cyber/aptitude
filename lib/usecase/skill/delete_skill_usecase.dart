64336dda20daa70d82715e5aa4915ff1edbad8a8
graciousnthushi <graciousnthushi@gmail.com>
2026-05-02 12:27:49 +0000

    feat/fixed errors

--- lib/usecase/skill/delete_skill_usecase.dart
+++ lib/usecase/skill/delete_skill_usecase.dart
@@ -15,38 +15,4 @@ class DeleteSkillUseCase {
 
     await userRepo.update(user.uid, user.toJson(), location: '', data: {});
   }
-}import 'package:myapp/core/data/repositories/user_repository.dart';
-
-class DeleteSkillUseCase {
-  Future<void> execute({
-    required String userId,
-    required String skillName,
-  }) async {
-    final userRepo = UserRepository();
-
-    final user = await userRepo.read(userId);
-
-    if (user == null) throw Exception("User not found");
-
-    user.skills.remove(skillName);
-
-    await userRepo.update(user.uid, user.toJson());
-  }
-}import 'package:myapp/core/data/repositories/user_repository.dart';
-
-class DeleteSkillUseCase {
-  Future<void> execute({
-    required String userId,
-    required String skillName,
-  }) async {
-    final userRepo = UserRepository();
-
-    final user = await userRepo.read(userId);
-
-    if (user == null) throw Exception("User not found");
-
-    user.skills.remove(skillName);
-
-    await userRepo.update(user.uid, user.toJson());
-  }
 }
